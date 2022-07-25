10e500ba364023ed626a7d5261791b5f2aed2175
Hilocv <101907782+Hilocv@users.noreply.github.com>
2022-07-25 14:26:36 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+BOT_TOKEN = os.environ.get('bot_token')
+API_ID = os.environ.get('api_id')
+API_HASH = os.environ.get('api_hash') 
+
+
+# DATOS DEL MOODLE
+USUARIO = os.environ.get('user')
+PASSWORD = os.environ.get('password')
+USUARIO_ID = os.environ.get('user_id')
+ZIP_MB = os.environ.get('zips_size')
+MOODLE_URL = os.environ.get('host')
+OWNER = [1906940331]
+OWNER2 = [1651372201]
+# CUENTA DE MEGA
+GMAIL_MEGA = 'diago8888diago@gmail.com'
+PASS_MEGA = 'Diagogoogle$4'
